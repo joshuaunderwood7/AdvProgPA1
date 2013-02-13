fe61f9419872971941baae8d411dfc8ce9167140
Joshua Underwood <joshuaunderwood7@gmail.com>
2013-02-13 16:58:13 -0700

    The Secant method produces the correct output when a = 6. this program is final

--- pa1.cpp
+++ pa1.cpp
@@ -20,7 +20,7 @@ int main(void)
     underwood::ENTER_TO_CONTINUE();
 
     //a=0, b=9, δ=10^-6, ε=10^-6, M=100 for the Secant method
-    underwood::Secant<double>(0,9,100,std::pow(double(10),-6),std::pow(double(10),-6));
+    underwood::Secant<double>(6,9,100,std::pow(double(10),-6),std::pow(double(10),-6));
     underwood::ENTER_TO_CONTINUE();
     return 0;
 }
